a98df8b17a4d68f7087cb9834c16c9461ddc70aa
Dane Thorsen <1124945-dthorsen@users.noreply.gitlab.com>
2020-06-20 12:20:27 -0500

    Added http fetcher

--- main.go
+++ main.go
@@ -4,24 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"regexp"
 	"strings"
 )
 
 func main() {
-	pattern := regexp.MustCompile(`^0\.0\.0\.0 [\S]+?$`)
-	f, err := os.Open("hosts")
+	resp, err := http.Get("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts")
 	if err != nil {
-		log.Fatalf("Error opening file hosts: %v", err)
+		log.Fatalf("failed to fetch SteveBlack list: %v", err)
 	}
-	s := bufio.NewScanner(f)
+	if resp.StatusCode != 200 {
+		log.Fatalf("failed to fetch SteveBlack list: %v", resp.Status)
+	}
+	pattern := regexp.MustCompile(`^0\.0\.0\.0 [\S]+?$`)
+
+	outputFile, err := os.Create("blocklist.txt")
+	if err != nil {
+		log.Fatalf("failed to create output file blocklist.txt: %v", err)
+	}
+
+	s := bufio.NewScanner(resp.Body)
 	for s.Scan() {
 		line := s.Text()
 		if pattern.MatchString(line) {
 			fields := strings.Split(line, " ")
-			fmt.Println(fields[1])
+			if fields[1] == "0.0.0.0" {
+				continue
+			}
+			_, err = outputFile.WriteString(fields[1] + "\n")
+			if err != nil {
+				log.Fatalf("failed to write to output file blocklist.txt: %v", err)
+			}
 		}
 	}
+	outputFile.Close()
+	resp.Body.Close()
+	fmt.Println("converted blocklist successfully")
 	return
 }
